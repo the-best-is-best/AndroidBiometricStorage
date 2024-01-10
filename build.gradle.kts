50cac09c5bf858d265c06093097359b0c426d86b
michelle raouf <michelle.raouf@outlook.com>
2024-01-10 23:01:51 +0200

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,30 @@
 plugins {
     id("com.android.application") version "8.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    `maven-publish`
+    id("maven-publish")
+    id("java")
 
+}
+apply(plugin = "maven-publish")
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            groupId = "com.tbib"
+            artifactId = "tbibbiometricstorage"
+            version = "1.0.0"
+        }
+    }
+    repositories {
+        maven {
+            url = uri("https://github.com/the-best-is-best/AndroidBiometricStorage")
+
+        }
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
